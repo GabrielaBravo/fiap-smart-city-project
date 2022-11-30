886f9d8a62e724f7ba8fed35d686513530425609
Gabriela Bravo Morinishi <gabrielabravo.usp@gmail.com>
2022-11-30 14:47:48 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/TipoProdutoController.cs
@@ -0,0 +1,97 @@
+﻿using FiapSmartCity.Models;
+using FiapSmartCity.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiapSmartCity.Controllers
+{
+    public class TipoProdutoController : Controller
+    {
+
+        private readonly TipoProdutoRepository TipoProdutoRepository;
+
+        public TipoProdutoController()
+        {
+            TipoProdutoRepository = new TipoProdutoRepository();
+        }
+
+        [Filtros.LogFilter]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var listaTipo = TipoProdutoRepository.Listar();
+            return View(listaTipo);
+        }
+
+        // Anotação de uso do Verb HTTP Get
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View(new TipoProduto());
+        }
+
+        // Anotação de uso do Verb HTTP Post
+        [HttpPost]
+        public ActionResult Cadastrar(Models.TipoProduto TipoProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                TipoProdutoRepository.Inserir(TipoProduto);
+
+                @TempData["mensagem"] = "Tipo cadastrado com sucesso!";
+                return RedirectToAction("Index", "TipoProduto");
+
+            }
+            else
+            {
+                return View(TipoProduto);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int Id)
+        {
+            var TipoProduto = TipoProdutoRepository.Consultar(Id);
+            return View(TipoProduto);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Models.TipoProduto TipoProduto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                TipoProdutoRepository.Alterar(TipoProduto);
+
+                @TempData["mensagem"] = "Tipo alterado com sucesso!";
+                return RedirectToAction("Index", "TipoProduto");
+            }
+            else
+            {
+                return View(TipoProduto);
+            }
+
+        }
+
+
+        [HttpGet]
+        public ActionResult Consultar(int Id)
+        {
+            var TipoProduto = TipoProdutoRepository.Consultar(Id);
+            return View(TipoProduto);
+        }
+
+
+        [HttpGet]
+        public ActionResult Excluir(int Id)
+        {
+            TipoProdutoRepository.Excluir(Id);
+
+            @TempData["mensagem"] = "Tipo removido com sucesso!";
+
+            return RedirectToAction("Index", "TipoProduto");
+        }
+
+
+
+    }
+}
---
+++ Repository/Context/DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using FiapSmartCity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapSmartCity.Repository.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<TipoProduto> TipoProduto { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("FiapSmartCityConnection"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
